44bd6ff3b320578f7c8d2f646df727866dba62fd
zhangxiang <13283347501@163.com>
2021-03-10 09:17:39 +0800

    添加组件

---
+++ src/components/form/components/zxCascader.vue
@@ -0,0 +1,53 @@
+/**
+* create by zhangxiang on 2021-02-23 13:41
+* 类注释：
+* 备注：
+*/
+<template>
+  <el-cascader
+      style="width: 100%;"
+      v-model="formData[item.prop]"
+      :placeholder="item.placeholder"
+      :style="item.style"
+      :class="item.class"
+      :options="item.options"
+      :disabled="item.disabled"
+      :show-all-levels="item.showAllLevels"
+      :separator="item.separator"
+      :collapse-tags="item.collapseTags"
+      :props="item.props"
+      :filterable="item.filterable"
+      :debounce="item.debounce"
+      :filter-method="item.filterMethod"
+      :clearable="item.clearable"
+      @change="change"
+  >
+  </el-cascader>
+</template>
+
+<script lang="ts">
+export default {
+  name: "zxCascader",
+  emits: ['event'],
+  props: {
+    item: {type: Object, default: {}},
+    formData: {},
+  },
+  setup(prop, context) {
+    const change = () => {
+      context.emit('event', {
+        type: 'change',
+        prop: prop.item.prop,
+        value: prop.formData[prop.item.prop]
+      })
+    }
+    return {
+      change,
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,8 @@ import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
 import 'dayjs/locale/zh-cn'
 import locale from 'element-plus/lib/locale/lang/zh-cn'
+import zxLib3 from 'zx-lib3'
 
 
 // @ts-ignore
-createApp(App).use(ElementPlus,{ locale }).use(store).use(router).mount('#app')
+createApp(App).use(ElementPlus,{ locale }).use(zxLib3).use(store).use(router).mount('#app')
